f558dee34d60c664901029084ab6c9d039ca6d09
turr4n <114745221+turr4n@users.noreply.github.com>
2023-03-12 18:23:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+const startButton = document.getElementById("js--startButton");
+const stopButton = document.getElementById("js--stopButton");
+const resetButton = document.getElementById("js--resetButton");
+const secondsDisplay = document.getElementById("js--seconds");
+const minutesDisplay = document.getElementById("js--minutes");
+const slider = document.getElementById("js--slider");
+const sliderText = document.getElementById("js--slider-text");
+const body = document.getElementById("js--body");
+const image = document.getElementById("js--imageView");
+const displayButton = document.getElementById("js--displayButton");
+
+function setFontSize(rem_size) {
+    slider.value = rem_size;
+    body.style.fontSize = `${slider.value}rem`;
+    sliderText.innerText = `${slider.value}x`;
+}
+
+setFontSize(2);
+
+let counterSec = 0; 
+let counterMin = 0; 
+
+let isRunning = false;
+let imageShown = false;
+let timer = null;
+
+let imageInfo = {
+    "alt": "een foto van een gloednoieuwe mooie mercedes",
+    "src": "/img/pexels-mike-b-120049.webp"
+};
+
+
+displayButton.onclick = function () {
+
+    if (!imageShown) {
+        image.setAttribute("src", imageInfo.src);
+        image.setAttribute("alt", imageInfo.alt);
+
+        displayButton.innerText = "Hide image"
+    } else {
+        image.setAttribute("src", "");
+        image.setAttribute("alt", "");
+
+        displayButton.innerText = "Display image"
+    }
+
+    imageShown = !imageShown;
+};
+
+slider.oninput = function() {
+    setFontSize(slider.value)
+};
+
+startButton.onclick = function () {
+    if (!isRunning) {
+
+        timer = setInterval(function(){
+            counterSec++; 
+
+            if (counterSec == 60) {
+                counterMin++;
+                counterSec = 0; 
+            }
+
+            
+            secondsDisplay.innerText = counterSec;
+            minutesDisplay.innerText = counterMin;
+
+        }, 1000)
+        
+        isRunning = true;
+    }
+
+}
+
+stopButton.onclick = function () {
+    if (timer == null || !isRunning) {
+        return
+    }
+
+    isRunning = false;
+    clearInterval(timer);
+}
+
+resetButton.onclick = function () {
+    counterSec = 0;
+    counterMin = 0;
+
+    secondsDisplay.innerHTML = 0;
+    minutesDisplay.innerHTML = 0
+}
